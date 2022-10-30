5f054d2ac2d5a1e508a8b26721210d74e33ff38f
Xan-Git <xan.s@outlook.com>
2022-10-30 18:21:06 -0500

    Completed initial working prototype of color functionality

---
+++ QRCodeViewer/Extensions.cs
@@ -0,0 +1,29 @@
+﻿namespace QRCodeViewer
+{
+    public static class Extensions
+    {
+        public static byte[] ColorToRGB(this Color color)
+        {
+            switch (color)
+            {
+                case Color.Red:
+                     return new byte[3] { 240, 10, 10 };
+
+                case Color.Green:
+                    return new byte[3] { 10, 240, 10 };
+
+                case Color.Blue:
+                    return new byte[3] { 10, 10, 240 };
+
+                case Color.White:
+                    return new byte[3] { 255, 255, 255 };
+
+                case Color.Black:
+                    return new byte[3] { 0, 0, 0 };
+
+                default:
+                    return new byte[3] { 128, 128, 128 };
+            }
+        }
+    }
+}
---
+++ QRCodeViewer/QrDto.cs
@@ -0,0 +1,25 @@
+﻿namespace QRCodeViewer
+{
+    public class QrDto
+    {
+        public string Url { get; set; } = "";
+
+        // Side length of square in pixels
+        public int Size { get; set; }
+
+        public Color LightColor { get; set; } = Color.White;
+
+        public Color DarkColor { get; set; } = Color.Black;
+    }
+
+    public enum Color
+    {
+        Red,
+        Blue,
+        Green,
+        White,
+        Black
+    }
+
+    
+}
